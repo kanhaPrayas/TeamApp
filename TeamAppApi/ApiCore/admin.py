31cab3f202ed3b227d2b5ff5b8c63638523932ef
Prayas Purusottam <prayas.purusottam@oracle.com>
2017-11-29 07:59:24 +0530

    added admin.py file

---
+++ TeamAppApi/ApiCore/admin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from .models import Team
+
+# Register your models here.
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+	search_fields = ('first_name','last_name','phone')
+	ordering = ('role','age')
